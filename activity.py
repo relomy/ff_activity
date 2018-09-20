c2f556a43a91e53885b60a500dc8e8e8682f6d4f
Adam Lewandowski <relomy@gmail.com>
2018-09-20 08:51:40 -0400

    added some logic to fix weird transactions + added pulling teams for each league

--- activity.py
+++ activity.py
@@ -5,80 +5,97 @@ from player_info import Player_Info
 class Activity(object):
     """Creates Activity object."""
 
-    def __init__(self, data):
+    def __init__(self, data, team_map):
         """Initialize Activity object."""
+
+        # hopefully figure out a better way to do this
+        self.team_map = team_map
 
         # this is where we will store the activity
         self.recent_activity = []
 
         # individual activity fields
         self.events = []
-        self.actingAsTeamOwner = data['actingAsTeamOwner']
-        self.activityType = int(data['activityType'])
-        self.batchProcessed = data['batchProcessed']
-        self.bidAmount = data['bidAmount']
-        self.byLM = data['byLM']
-        self.date = data['date']
-        self.dateAccepted = data['dateAccepted']
-        self.dateModified = data['dateModified']
-        self.dateProposed = data['dateProposed']
-        self.dateToProcess = data['dateToProcess']
-        self.parentPendingMoveBatchId = data['parentPendingMoveBatchId']
-        self.pendingMoveBatchId = data['pendingMoveBatchId']
-        self.pendingMoveItems = data['pendingMoveItems']
 
-        # if self.pendingMoveItems:
-        #     self.pendingMoveItemsHuman = self.get_human(self.pendingMoveItems)
-            # print(self.pendingMoveItemsHuman)
-        self.pendingMoveItemsCount = len(self.pendingMoveItems)
-        # {   'draftOverallSelection': 0,
-        #                             'fromSlotCategoryId': 1001,
-        #                             'fromTeamId': -1,
-        #                             'keeper': False,
-        #                             'moveTypeId': 2,
-        #                             'playerId': 60008,
-        #                             'rating': 0,
-        #                             'toSlotCategoryId': 20,
-        #                             'toTeamId': 5},
-        #
-        self.proposingTeamId = data['proposingTeamId']
-        self.proposingUserProfileId = data['proposingUserProfileId']
-        self.rating = data['rating']
-        self.scoringPeriodToProcess = data['scoringPeriodToProcess']
-        self.skipTransactionCounters = data['skipTransactionCounters']
-        self.statusId = data['statusId']
-        self.teamsAcceptedTrade = data['teamsAcceptedTrade']
-        self.teamsInvolved = data['teamsInvolved']
-        self.teamsVotedApproveTrade = data['teamsVotedApproveTrade']
-        self.teamsVotedVetoTrade = data['teamsVotedVetoTrade']
-        self.tradeProposalExpirationDays = data['tradeProposalExpirationDays']
-        self.transactionLogItemTypeId = data['transactionLogItemTypeId']
-        self.typeId = int(data['typeId'])
-        self.userProfileId = data['userProfileId']
-        self.usersProtestTrade = data['usersProtestTrade']
+        # check activityType
+        try:
+            self.activityType = int(data['activityType'])
+        except ValueError:
+            exit("data['activityType'] cannot be converted to an integer")
+
+        # an activityType of 0 appears to be just a message
+        if self.activityType == 0:
+            self.date = data['date']
+            self.subject = data['subject']
+            self.body = data['body']
+        else:
+            # if activityType is not 0, we assume all of these fields are valid
+            self.actingAsTeamOwner = data['actingAsTeamOwner']
+            self.batchProcessed = data['batchProcessed']
+            self.bidAmount = data['bidAmount']
+            self.byLM = data['byLM']
+            self.date = data['date']
+            self.dateAccepted = data['dateAccepted']
+            self.dateModified = data['dateModified']
+            self.dateProposed = data['dateProposed']
+            self.dateToProcess = data['dateToProcess']
+            self.parentPendingMoveBatchId = data['parentPendingMoveBatchId']
+            self.pendingMoveBatchId = data['pendingMoveBatchId']
+            self.pendingMoveItems = data['pendingMoveItems']
+
+            # if self.pendingMoveItems:
+            #     self.pendingMoveItemsHuman = self.get_human(self.pendingMoveItems)
+                # print(self.pendingMoveItemsHuman)
+            self.pendingMoveItemsCount = len(self.pendingMoveItems)
+            # {   'draftOverallSelection': 0,
+            #                             'fromSlotCategoryId': 1001,
+            #                             'fromTeamId': -1,
+            #                             'keeper': False,
+            #                             'moveTypeId': 2,
+            #                             'playerId': 60008,
+            #                             'rating': 0,
+            #                             'toSlotCategoryId': 20,
+            #                             'toTeamId': 5},
+            #
+            self.proposingTeamId = data['proposingTeamId']
+            self.proposingUserProfileId = data['proposingUserProfileId']
+            self.rating = data['rating']
+            self.scoringPeriodToProcess = data['scoringPeriodToProcess']
+            self.skipTransactionCounters = data['skipTransactionCounters']
+            self.statusId = data['statusId']
+            self.teamsAcceptedTrade = data['teamsAcceptedTrade']
+            self.teamsInvolved = data['teamsInvolved']
+            self.teamsVotedApproveTrade = data['teamsVotedApproveTrade']
+            self.teamsVotedVetoTrade = data['teamsVotedVetoTrade']
+            self.tradeProposalExpirationDays = data['tradeProposalExpirationDays']
+            self.transactionLogItemTypeId = data['transactionLogItemTypeId']
+            self.typeId = int(data['typeId'])
+            self.userProfileId = data['userProfileId']
+            self.usersProtestTrade = data['usersProtestTrade']
         # self.name = data['leaguesettings']['name']
         # print("__init__ Activity object")
 
     def get_human(self, move_items):
         # mapping IDs/types
         move_type_map = {
+            0: '???',
             2: 'Add',
             3: 'Drop',
             5: 'Draft',
         }
-        team_map = {
-            1: 'Ryne',
-            2: 'Morgan',
-            3: 'Kenny',
-            4: 'Bobby',
-            5: 'Jordan',
-            6: 'Cassady',
-            7: 'Adam',
-            8: 'Shannon',
-            9: 'Cody',
-            10: 'Chris',
-            -1: 'FA/Waivers/Null'
-        }
+        # team_map = {
+        #     1: 'Ryne',
+        #     2: 'Morgan',
+        #     3: 'Kenny',
+        #     4: 'Bobby',
+        #     5: 'Jordan',
+        #     6: 'Cassady',
+        #     7: 'Adam',
+        #     8: 'Shannon',
+        #     9: 'Cody',
+        #     10: 'Chris',
+        #     -1: 'FA/Waivers/Null'
+        # }
         roster_map = {0: 'QB', 1: 'TQB',
                       2: 'RB', 3: 'RB/WR',
                       4: 'WR', 5: 'WR/TE',
@@ -119,8 +136,8 @@ class Activity(object):
             print(playa)
             l.append("moveType: {}".format(move_type_map.get(x['moveTypeId'], 'unknown move_type ???')))
             l.append("from team [{}] to team [{}]".format(
-                team_map[x['fromTeamId']],
-                team_map[x['toTeamId']]))
+                self.team_map[x['fromTeamId']],
+                self.team_map[x['toTeamId']]))
             l.append("from slot [{}] --> [{}]".format(
                 roster_map[x['fromSlotCategoryId']],
                 roster_map[x['toSlotCategoryId']]))
@@ -132,20 +149,12 @@ class Activity(object):
         return 'Activity({0})'.format(self.transactionLogItemTypeId)
 
     def __str__(self):
+        # if activity_type is 0, return date/subject/body
+        if self.activityType == 0:
+            return ' '.join([self.date, self.subject, self.body])
+
+        # otherwise, give some ID mappings
         move_type_map = {2: 'Add', 3: 'Drop', 5: 'Draft'}
-        team_map = {
-            1: 'Ryne',
-            2: 'Morgan',
-            3: 'Kenny',
-            4: 'Bobby',
-            5: 'Jordan',
-            6: 'Cassady',
-            7: 'Adam',
-            8: 'Shannon',
-            9: 'Cody',
-            10: 'Chris',
-            -1: 'FA/Waivers/Null'
-        }
         roster_map = {0: 'QB', 1: 'TQB',
                       2: 'RB', 3: 'RB/WR',
                       4: 'WR', 5: 'WR/TE',
@@ -159,11 +168,27 @@ class Activity(object):
                       20: 'BE', 21: 'IR',
                       22: '', 23: 'RB/WR/TE',
                       1001: 'FA', 1002: 'Waivers'}
+
+        # team_map = {
+        #     1: 'Ryne',
+        #     2: 'Morgan',
+        #     3: 'Kenny',
+        #     4: 'Bobby',
+        #     5: 'Jordan',
+        #     6: 'Cassady',
+        #     7: 'Adam',
+        #     8: 'Shannon',
+        #     9: 'Cody',
+        #     10: 'Chris',
+        #     -1: 'FA/Waivers/Null'
+        # }
+
         date = self.date
         activity_type = move_type_map.get(self.activityType, 'unknown activityType???')
+
         teams_involved = []
         for team in self.teamsInvolved:
-            teams_involved.append(team_map.get(team, 'unknown team???'))
+            teams_involved.append(self.team_map.get(team, 'unknown team???'))
 
         # include basic text for involved teams
         text = ["[{0}] Transaction [{1}] involving team {2}".format(
@@ -175,11 +200,11 @@ class Activity(object):
             for item in self.pendingMoveItems:
                 player = Player_Info(item['playerId'])
                 move = 'Team {0} moved {1} [{2}] from {3} to Team {4}'.format(
-                    team_map.get(item['fromTeamId'], 'unknown team'),
+                    self.team_map.get(item['fromTeamId'], 'unknown team'),
                     player,
                     roster_map.get(item['fromSlotCategoryId'], 'unknown fromSlotCategoryId'),
                     roster_map.get(item['toSlotCategoryId'], 'unknown toSlotCategoryId'),
-                    team_map.get(item['toTeamId'], 'unknown team')
+                    self.team_map.get(item['toTeamId'], 'unknown team')
                 )
                 text.append('---- {0}'.format(move))
 
