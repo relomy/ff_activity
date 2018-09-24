7fe827e77f97157fd82103eff6ae0f65404337b0
Adam Lewandowski <relomy@gmail.com>
2018-09-24 14:30:00 -0400

    added transaction_log_type and modified __str__ to closer match ESPN transactions

--- activity.py
+++ activity.py
@@ -1,4 +1,5 @@
-import pprint
+from dateutil import parser
+
 from player_info import Player_Info
 
 
@@ -8,6 +9,7 @@ class Activity(object):
     def __init__(self, data, team_map):
         """Initialize Activity object."""
 
+        self.data = data
         # hopefully figure out a better way to do this
         self.team_map = team_map
 
@@ -42,21 +44,7 @@ class Activity(object):
             self.parentPendingMoveBatchId = data['parentPendingMoveBatchId']
             self.pendingMoveBatchId = data['pendingMoveBatchId']
             self.pendingMoveItems = data['pendingMoveItems']
-
-            # if self.pendingMoveItems:
-            #     self.pendingMoveItemsHuman = self.get_human(self.pendingMoveItems)
-                # print(self.pendingMoveItemsHuman)
             self.pendingMoveItemsCount = len(self.pendingMoveItems)
-            # {   'draftOverallSelection': 0,
-            #                             'fromSlotCategoryId': 1001,
-            #                             'fromTeamId': -1,
-            #                             'keeper': False,
-            #                             'moveTypeId': 2,
-            #                             'playerId': 60008,
-            #                             'rating': 0,
-            #                             'toSlotCategoryId': 20,
-            #                             'toTeamId': 5},
-            #
             self.proposingTeamId = data['proposingTeamId']
             self.proposingUserProfileId = data['proposingUserProfileId']
             self.rating = data['rating']
@@ -72,6 +60,7 @@ class Activity(object):
             self.typeId = int(data['typeId'])
             self.userProfileId = data['userProfileId']
             self.usersProtestTrade = data['usersProtestTrade']
+
         # self.name = data['leaguesettings']['name']
         # print("__init__ Activity object")
 
@@ -154,7 +143,9 @@ class Activity(object):
             return ' '.join([self.date, self.subject, self.body])
 
         # otherwise, give some ID mappings
-        move_type_map = {2: 'Add', 3: 'Drop', 5: 'Draft'}
+        move_type_map = {2: 'Add', 3: 'Drop', 4: 'Trade', 5: 'Draft'}
+
+        # roster map for slot IDs
         roster_map = {0: 'QB', 1: 'TQB',
                       2: 'RB', 3: 'RB/WR',
                       4: 'WR', 5: 'WR/TE',
@@ -168,7 +159,15 @@ class Activity(object):
                       20: 'BE', 21: 'IR',
                       22: '', 23: 'RB/WR/TE',
                       1001: 'FA', 1002: 'Waivers'}
-
+        # tra
+        transaction_log_type_map = {
+            1: "Waiver Add",
+            2: "Add",
+            3: "Drop",
+            4: "Trade Completed",
+            5: "Draft",
+            11: "Trade Accepted"
+        }
         # team_map = {
         #     1: 'Ryne',
         #     2: 'Morgan',
@@ -183,30 +182,58 @@ class Activity(object):
         #     -1: 'FA/Waivers/Null'
         # }
 
-        date = self.date
-        activity_type = move_type_map.get(self.activityType, 'unknown activityType???')
+        # convert date to datetime
+        date = parser.parse(self.date)
+
+
+        transaction_type = transaction_log_type_map.get(self.transactionLogItemTypeId, 'unknown transactionLogItemTypeId???')
 
         teams_involved = []
         for team in self.teamsInvolved:
             teams_involved.append(self.team_map.get(team, 'unknown team???'))
 
         # include basic text for involved teams
-        text = ["[{0}] Transaction [{1}] involving team {2}".format(
+        text = ["[{0:%Y-%m-%d %H:%M:%S}] {1} - {2}".format(
             date,
-            activity_type,
+            transaction_type,
             '/'.join(teams_involved))]
+
+        # if this is a waiver add, put bid amount
+        if self.transactionLogItemTypeId == 1:
+            text[0] = text[0].replace("Waiver Add", "Waiver Add - Bid amount: ${0}".format(self.bidAmount))
+
         # check for pending move items and include those too
         if self.pendingMoveItemsCount and self.pendingMoveItemsCount > 0:
             for item in self.pendingMoveItems:
+                # print(item)
                 player = Player_Info(item['playerId'])
-                move = 'Team {0} moved {1} [{2}] from {3} to Team {4}'.format(
-                    self.team_map.get(item['fromTeamId'], 'unknown team'),
-                    player,
-                    roster_map.get(item['fromSlotCategoryId'], 'unknown fromSlotCategoryId'),
-                    roster_map.get(item['toSlotCategoryId'], 'unknown toSlotCategoryId'),
-                    self.team_map.get(item['toTeamId'], 'unknown team')
-                )
-                text.append('---- {0}'.format(move))
+                # print("moveTypeId: {} [{}]".format(item['moveTypeId'], move_type_map.get(item['moveTypeId'], '???')))
+                # if moving TO waivers/FA/null
+                if item['toTeamId'] == -1:
+                    move = '{0} dropped {1} [{2}] to {3}'.format(
+                        self.team_map.get(item['fromTeamId'], 'unknown team'),
+                        player,
+                        roster_map.get(item['fromSlotCategoryId'], 'unknown fromSlotCategoryId'),
+                        roster_map.get(item['toSlotCategoryId'], 'unknown toSlotCategoryId')
+                    )
+                # if moving FROM waivers/FA/null
+                elif item['fromTeamId'] == -1:
+                    move = '{0} added {1} [{2}] from {3}'.format(
+                        self.team_map.get(item['toTeamId'], 'unknown team'),
+                        player,
+                        roster_map.get(item['toSlotCategoryId'], 'unknown toSlotCategoryId'),
+                        roster_map.get(item['fromSlotCategoryId'], 'unknown fromSlotCategoryId')
+                    )
+                # trades and others?
+                else:
+                    move = '{0} moved {1} [{2}] from {3} to {4}'.format(
+                        self.team_map.get(item['fromTeamId'], 'unknown team'),
+                        player,
+                        roster_map.get(item['fromSlotCategoryId'], 'unknown fromSlotCategoryId'),
+                        roster_map.get(item['toSlotCategoryId'], 'unknown toSlotCategoryId'),
+                        self.team_map.get(item['toTeamId'], 'unknown team')
+                    )
+                text.append('{0}'.format(move))
 
         return '\n'.join(text)
 
